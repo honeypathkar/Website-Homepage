030c986329c09643168d0f47679f772debb79f84
honeypatkar <honeypatkar70@gmail.com>
2024-06-01 17:21:30 +0530

    Form Submission Complete

---
+++ index.js
@@ -0,0 +1,8 @@
+function openPopup() {
+  document.getElementById("myPopup").style.display = "block";
+  console.log("Form is Open")
+}
+
+function closePopup() {
+  document.getElementById("myPopup").style.display = "none";
+}
